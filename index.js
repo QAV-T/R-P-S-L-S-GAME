d002634b6bdb39ca5c2192d2f5f06dc8d30664ae
QAV-T S <safieh.tariq@gmail.com>
2024-04-19 10:31:06 +0000

    computer turn and choiceBtn

--- index.js
+++ index.js
@@ -8,3 +8,36 @@ const choiceBtn = document.querySelectorAll(".choiceBtn");
 let player;
 let computer;
 let result;
+
+choiceBtn.forEach(button => button.addEventListener("click", () => {
+             
+    player = button.textContent
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+
+
+}) );
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 5) + 1;
+    switch(randNum){
+        case 1 :
+            computer = 'ROCK';
+            break;
+        case 2 :
+            computer = 'PAPER';
+            break;
+        case 3 :
+            computer = 'SCISSOR';
+            break; 
+        case 4 :
+            computer = 'SPOCK';
+                break;            
+        
+        case 5 :
+            computer = 'LIZZARD';
+            break;            
+    }               
+    }
