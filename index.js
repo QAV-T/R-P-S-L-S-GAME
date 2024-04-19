4f520c8271f71f03f62d5bd7ad639ca1aa70816d
QAV-T S <safieh.tariq@gmail.com>
2024-04-19 09:59:30 +0000

    js consts and lets

---
+++ index.js
@@ -0,0 +1,10 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelectorAll(".choiceBtn");
+//const treisLeft = document.querySelectorAll("#triesLeft");
+//const score = document.querySelectorAll("#score");
+
+let player;
+let computer;
+let result;
